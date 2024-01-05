76d70b50998504d55ddacdbc1361a83bae20f8a6
Barry <a.salambarry@gmail.com>
2024-01-05 23:12:56 +0100

    validation de la reponse et gestion du score

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Levels from '../Levels/Levels';
 import ProgresBar from '../ProgressBar/ProgresBar';
 import { MarvelQuizData } from '../MarvelQuizData/MarvelQuizData';
+
 class Quiz extends React.Component {
 
 	state = {
@@ -14,9 +15,12 @@ class Quiz extends React.Component {
 		question: null,
 		options: [],
 		idQuestion: 0,
-		btnDisabled:true,
-		userAnswer:null
+		btnDisabled: true,
+		// La reponse donnée par l'utilisateur
+		userAnswer: null,
+		score: 0
 	}
+	storedDataRef = React.createRef()
 
 	// Une fonction pour chahrger nos données depuis le fichier MarvelQuizData
 	loadQuestions = (quizz) => {
@@ -25,6 +29,8 @@ class Quiz extends React.Component {
 		// console.log(fetchArrayData);
 		// je fait une condtion pour verifier est ce on n'a suffisament de data
 		if (fetchArrayQuizz.length >= this.state.maxLenghtData) {
+			this.storedDataRef.current = fetchArrayQuizz
+			// console.log(this.storedDataRef.current);
 			// Le destructuring pour recuperer toutes les questions exceptés de ses réponses
 			const newArray = fetchArrayQuizz.map(({ answer, ...keepRest }) => keepRest)
 			console.log(newArray);
@@ -33,6 +39,27 @@ class Quiz extends React.Component {
 			})
 		} else {
 			console.log("Pas suffisament de données pour afficher");
+		}
+	}
+	// Une fonction qui va nous permettre de passer aux questions suivantes
+	nextQuestion = () => {
+		// Une condition pour verifier qu'on n'a pas depasser nos 10 questions au niveau debutant
+		if (this.state.idQuestion === this.state.maxLenghtData - 1) {
+			// Le quizz est terminé
+		} else {
+			this.setState(prevState => ({
+				idQuestion: prevState.idQuestion + 1
+			}))
+		}
+		// Gestion du score
+		// creation d'une variable pour recuperer la reponse qui à été selectionner par les users
+		const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer
+
+		// Une condition pour verifier est ce que la reponse qui à été selectionner par le user est egale à ce qui est dans le data
+		if(this.state.userAnswer === goodAnswer){
+			this.setState(prevState =>({
+				score: prevState.score + 1
+			}))
 		}
 	}
 	componentDidMount() {
@@ -44,24 +71,33 @@ class Quiz extends React.Component {
 				question: this.state.storedQuestions[this.state.idQuestion].question,
 				options: this.state.storedQuestions[this.state.idQuestion].options,
 			})
+		}
+		if (this.state.idQuestion !== prevState.idQuestion) {
+			this.setState({
+				question: this.state.storedQuestions[this.state.idQuestion].question,
+				options: this.state.storedQuestions[this.state.idQuestion].options,
+				userAnswer: null,
+				btnDisabled: true,
 
+			})
 		}
 	}
 	submitAnswer = (selectedAnswer) => {
 		this.setState({
-			userAnswer:selectedAnswer,
-			btnDisabled:false
+			userAnswer: selectedAnswer,
+			btnDisabled: false
 		})
 	}
+	
 	render() {
 		// const { pseudo } = this.props.userData;
-		const dispalayOptions = this.state.options.map((option, index)=>{
+		const dispalayOptions = this.state.options.map((option, index) => {
 			return (
 				// <p className='answerOptions selected' onClick={()=> this.submitAnswer(option)}>{option}</p>
-				<p className={`answerOptions ${this.state.userAnswer === option ? "selected" : null}`} onClick={()=> this.submitAnswer(option)}>{option}</p>
+				<p className={`answerOptions ${this.state.userAnswer === option ? "selected" : null}`} onClick={() => this.submitAnswer(option)}>{option}</p>
 			)
 		})
-		
+
 		return (
 			<div>
 				<Levels />
@@ -72,7 +108,7 @@ class Quiz extends React.Component {
 				<p className='answerOptions'>Question 3</p>
 				<p className='answerOptions'>Question 4</p> */}
 				{dispalayOptions}
-				<button disabled={this.state.btnDisabled} className='btnSubmit'>Suivant</button>
+				<button disabled={this.state.btnDisabled} className='btnSubmit' onClick={this.nextQuestion}>Suivant</button>
 			</div>
 		);
 	}
